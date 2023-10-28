0870ea84beaba631daa7793c304d10fcaf226657
nightsurfer88 <149131871+nightsurfer88@users.noreply.github.com>
2023-10-27 21:50:44 -0700

    Update events.js

--- events.js
+++ events.js
@@ -1,18 +1,13 @@
 AFRAME.registerComponent('markerhandler', {
-
-    init: function() {
+    init: function () {
         const animatedMarker = document.querySelector("#animated-marker");
         const videoElement = document.querySelector("#video");
 
         // Add a click event listener to the animatedMarker
-        animatedMarker.addEventListener('click', function() {
+        animatedMarker.addEventListener('click', function () {
             if (videoElement) {
-                if (videoElement.paused) {
-                    videoElement.play(); // Play the video if it's paused
-                } else {
-                    videoElement.pause(); // Pause the video if it's playing
-                }
+                videoElement.play(); // Play the video
             }
         });
-    }
+    },
 });
